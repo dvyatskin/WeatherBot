6aec1a6a8bab828f06589811f3f34edb0a560e66
Vyatskin Dinar <vyatskin@gmail.com>
2024-01-26 22:42:19 +0300

    Добавил правильную обработку города в команде /low

--- handlers/custom_handlers/low.py
+++ handlers/custom_handlers/low.py
@@ -1,4 +1,5 @@
 from telebot.types import Message
+from telebot import types
 
 from loader import bot, API
 import requests
@@ -15,11 +16,28 @@ def choose_city(message: Message) -> None:
 @bot.message_handler(state=WeatherInfoState.low)
 def get_min_temp(message: Message) -> None:
     city_name_min = message.text.strip()
-    res = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city_name_min}&appid={API}&units=metric')
+    limit = 3
+    res = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name_min}&limit={limit}&appid={API}')
     if res.status_code == 200:
         data = json.loads(res.text)
-        bot.reply_to(message, f'Минимальная температура в городе {city_name_min}:\n'
-                              f'tmin={round(data["main"]["temp_min"], 1)}')
-    else:
-        bot.reply_to(message, 'Город не найден')
-    bot.set_state(message.from_user.id, WeatherInfoState, message.chat.id)
+        markup = types.InlineKeyboardMarkup()
+        btn1 = types.InlineKeyboardButton(f'{data[0]["name"]}, {data[0]["country"]}', callback_data='first')
+        markup.row(btn1)
+        btn2 = types.InlineKeyboardButton(f'{data[1]["name"]}, {data[1]["country"]}', callback_data='second')
+        btn3 = types.InlineKeyboardButton(f'{data[2]["name"]}, {data[2]["country"]}', callback_data='third')
+        markup.row(btn2, btn3)
+        bot.send_message(message.chat.id, f'Выберите город:', reply_markup=markup)
+
+        @bot.callback_query_handler(func=lambda callback: True)
+        def callback_message(callback):
+            if callback.data == 'first':
+                bot.send_message(callback.message.chat.id, 'Первый')
+            elif callback.data == 'second':
+                bot.send_message(callback.message.chat.id, 'ВТорой')
+            elif callback.data == 'third':
+                bot.send_message(callback.message.chat.id, 'Третий')
+    #     bot.reply_to(message, f'Минимальная температура в городе {city_name_min}:\n'
+    #                           f'tmin={round(data["main"]["temp_min"], 1)}')
+    # else:
+    #     bot.reply_to(message, 'Город не найден')
+    # bot.set_state(message.from_user.id, WeatherInfoState, message.chat.id)

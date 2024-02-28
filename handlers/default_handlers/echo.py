fbf059ca4359dbc54e65ec435c42dab08ddb7ce1
Vyatskin Dinar <vyatskin@gmail.com>
2024-02-28 16:00:17 +0300

    Исправил Эхо

--- handlers/default_handlers/echo.py
+++ handlers/default_handlers/echo.py
@@ -9,14 +9,6 @@ from loader import bot
 # Эхо хендлер, куда летят текстовые сообщения без указанного состояния
 @bot.message_handler(state=None)
 def bot_echo(message: Message):
-    command = 'без состояния'
-
-    conn = sqlite3.connect(os.path.abspath(os.path.join('database', 'db_history')))
-    cur = conn.cursor()
-    cur.execute("INSERT INTO history (command, city) VALUES ('%s', '%s')" % (command, message.text))
-    conn.commit()
-    cur.close()
-    conn.close()
 
     bot.reply_to(
         message, f"Извините, я вас не понимаю!🤕\n"
